9ba408befda0ee6ee9e5562149e2267ebba1c003
plater <davejplater@gmail.com>
2022-09-25 12:35:01 +0200

    initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ssd1306_mpl.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ssd1306_mpl.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ssd1306mpl.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ssd1306mpl.x.tar
